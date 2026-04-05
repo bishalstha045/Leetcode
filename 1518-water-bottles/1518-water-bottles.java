21e52b7e5dcca737b14c9f9ec544ec1760cc6a5b
Bishal Shrestha <bishalstha045@gmail.com>
2026-04-05 22:20:35 +0530

    Time: 0 ms (100%), Space: 42 MB (66.41%) - LeetHub

---
+++ 1518-water-bottles/1518-water-bottles.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int numWaterBottles(int numBottles, int numExchange) {
+        int total=numBottles;
+        int empty=numBottles;
+        while(empty>=numExchange){
+            int newbottle=empty/numExchange;
+            total+=newbottle;
+            empty=newbottle+(empty%numExchange);
+        }
+        return total;
+    }
+}
