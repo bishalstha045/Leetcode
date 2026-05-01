4ad31bb5e660b1b0df380029ea7f4080dfa2176a
Bishal Shrestha <bishalstha045@gmail.com>
2026-05-01 23:10:55 +0530

    Time: 5 ms (42.61%), Space: 42.5 MB (84.72%)

---
+++ 0441-arranging-coins/0441-withoutbinarysearch.java
@@ -0,0 +1,13 @@
+
+  class Solution {
+    public int arrangeCoins(int n) {
+        int row=1;
+        while(row<=n){
+        n=n-row;
+        row++;
+        }
+        return row-1;
+    }
+}
+
+// i will do it again using binary search, binary search results in giving O(log n).
