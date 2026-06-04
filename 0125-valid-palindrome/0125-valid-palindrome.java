b38d33226eeb2b8f69fc586500d237c0be1ca9d6
Bishal Shrestha <bishalstha045@gmail.com>
2026-06-04 23:48:21 +0530

    Time: 3 ms (60.56%), Space: 44.9 MB (43.61%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -1,36 +1,25 @@
 class Solution {
     public boolean isPalindrome(String s) {
-
-        StringBuilder str = new StringBuilder();
-
-        for(int i = 0; i < s.length(); i++) {
-
+        StringBuilder sc= new StringBuilder();
+        for(int i=0; i<s.length();i++){
             char ch = s.charAt(i);
-
-            // convert uppercase to lowercase
-            if(ch >= 'A' && ch <= 'Z') {
+            //1 step is to convert uppercase to lowercase considering ascii value for A= 65 and a=97
+            if(ch>='A' && ch<='Z'){
                 ch = (char)(ch + 32);
             }
-
-            // keep only letters and digits
-            if((ch >= 'a' && ch <= 'z') || (ch >= '0' && ch <= '9')) {
-                str.append(ch);
+            // 2nd step is to remove space and special characters
+            if((ch>='a' && ch<='z') || (ch>='0' && ch<='9')){
+                sc.append(ch); 
             }
         }
-
-        int i = 0;
-        int j = str.length() - 1;
-
-        while(i < j) {
-
-            if(str.charAt(i) != str.charAt(j)) {
-                return false;
+            int i=0;
+            int j=sc.length()-1;
+            while(i<=j){
+                if(sc.charAt(i)!=sc.charAt(j)) return false;
+                i++;
+                j--;
             }
-
-            i++;
-            j--;
-        }
-
-        return true;
+            return true;
     }
 }
+// firstly you have to convert upper into lower case then you have to simple remove special character like , and other and space too then finally check palindrome or not using two pointer strategy for that we have use two pointers i and j
