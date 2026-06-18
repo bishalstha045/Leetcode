cfa06d24fbf7f32fd20aaaf543d32dcfaeef9f6b
Bishal Shrestha <bishalstha045@gmail.com>
2026-06-18 23:24:52 +0530

    Time: 0 ms (100%), Space: 43.7 MB (68.61%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.java
+++ 0048-rotate-image/0048-rotate-image.java
@@ -1,7 +1,6 @@
 class Solution {
     public void rotate(int[][] matrix) {
         int n = matrix.length;
-
         // Transpose the matrix
         for (int i = 0; i < n; i++) {
             for (int j = i; j < n; j++) {
@@ -10,7 +9,6 @@ class Solution {
                 matrix[j][i] = temp;
             }
         }
-
         // Reverse each row
         for (int i = 0; i < n; i++) {
             int left = 0;
