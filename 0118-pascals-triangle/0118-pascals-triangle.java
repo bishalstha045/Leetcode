1b73538d71c0b70ec4f3780718cd4aa443ec8390
Bishal Shrestha <bishalstha045@gmail.com>
2026-06-20 23:22:25 +0530

    Time: 1 ms (93.96%), Space: 43.4 MB (69.31%) - LeetHub

--- 0118-pascals-triangle/0118-pascals-triangle.java
+++ 0118-pascals-triangle/0118-pascals-triangle.java
@@ -1,22 +1,22 @@
 class Solution {
     public List<List<Integer>> generate(int numRows) {
-        List<List<Integer>> triangle = new ArrayList<>();
-        for(int i = 0; i < numRows; i++) {
+        List<List<Integer>> traingle = new ArrayList<>();
+        for(int i=0;i<numRows;i++){
             List<Integer> row = new ArrayList<>();
-            for(int j = 0; j <= i; j++) {
-                // Boundary condition
-                if(j == 0 || j == i) {
+            for(int j=0;j<=i;j++){
+                if(j==0 || j==i){
                     row.add(1);
                 }
-                else {
-                    row.add(
-                        triangle.get(i - 1).get(j - 1)
-                        + triangle.get(i - 1).get(j)
-                    );
+                else{
+                    row.add(traingle.get(i-1).get(j) + traingle.get(i-1).get(j-1));
                 }
             }
-            triangle.add(row);
+             traingle.add(row);
         }
-        return triangle;
+        return traingle;
     }
 }
+
+// so it is a simple 2d arraylist question, just remember cases:
+// case 1 : for corner elements its always 1 i.e when j=0 and j=i its always 1
+// caase 2: for middle element , go to previous row and add elment and go to left 
