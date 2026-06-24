a7e48dc4f5d720b61c312dce2e73a3574c1d659a
Bishal Shrestha <bishalstha045@gmail.com>
2026-06-24 23:28:27 +0530

    Time: 3 ms (98.23%), Space: 47.2 MB (20%) - LeetHub

--- 0885-spiral-matrix-iii/0885-spiral-matrix-iii.java
+++ 0885-spiral-matrix-iii/0885-spiral-matrix-iii.java
@@ -1,44 +1,40 @@
 class Solution {
-    public int[][] spiralMatrixIII(int rows, int cols,int rStart, int cStart) {
-        int[][] ans = new int[rows * cols][2];
-        int count = 0;
-        ans[count++] = new int[]{rStart, cStart};
-        int steps = 1;
-        while (count < rows * cols) {
-            // Right
-            for (int i = 0; i < steps; i++) {
+    public int[][] spiralMatrixIII(int rows, int cols, int rStart, int cStart) {
+        int [][] ans = new int [rows * cols][2];
+        int count=0;
+        ans[count++] = new int []{rStart, cStart};
+        int step=1;
+        while(count< rows*cols){
+            //right
+            for(int i=0;i<step;i++){
                 cStart++;
-                if (rStart >= 0 && rStart < rows &&
-                    cStart >= 0 && cStart < cols) {
-                    ans[count++] = new int[]{rStart, cStart};
+                if(rStart>=0 && rStart<rows &&  cStart>=0 && cStart<cols){
+                    ans[count++] = new int []{rStart, cStart};
                 }
             }
-            // Down
-            for (int i = 0; i < steps; i++) {
+            //down
+            for(int i=0;i<step;i++){
                 rStart++;
-                if (rStart >= 0 && rStart < rows &&
-                    cStart >= 0 && cStart < cols) {
-                    ans[count++] = new int[]{rStart, cStart};
+                if(rStart>=0 && rStart<rows &&  cStart>=0 && cStart<cols){
+                    ans[count++] = new int []{rStart, cStart};
                 }
             }
-            steps++;
-            // Left
-            for (int i = 0; i < steps; i++) {
+            step++;
+            //left
+            for(int i=0;i<step;i++){
                 cStart--;
-                if (rStart >= 0 && rStart < rows &&
-                    cStart >= 0 && cStart < cols) {
-                    ans[count++] = new int[]{rStart, cStart};
+                if(rStart>=0 && rStart<rows &&  cStart>=0 && cStart<cols){
+                    ans[count++] = new int []{rStart, cStart};
                 }
             }
-            // Up
-            for (int i = 0; i < steps; i++) {
+            //up
+            for(int i=0;i<step;i++){
                 rStart--;
-                if (rStart >= 0 && rStart < rows &&
-                    cStart >= 0 && cStart < cols) {
-                    ans[count++] = new int[]{rStart, cStart};
+                if(rStart>=0 && rStart<rows &&  cStart>=0 && cStart<cols){
+                    ans[count++] = new int []{rStart, cStart};
                 }
             }
-            steps++;
+            step++;
         }
         return ans;
     }
