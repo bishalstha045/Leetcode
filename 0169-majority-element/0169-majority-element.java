da1f2b7ca550d4decc6c11fa0ddd279897aab5fe
Bishal Shrestha <bishalstha045@gmail.com>
2026-07-05 17:45:40 +0530

    Time: 1 ms (99.89%), Space: 55.9 MB (6.47%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -1,15 +1,37 @@
+
+// Boyer-Moore Voting Algorithm
 class Solution {
     public int majorityElement(int[] nums) {
-        int n=nums.length;
-        HashMap<Integer,Integer> map =new HashMap<>();
-        for(int el:nums){
-            map.put(el,map.getOrDefault(el,0)+1);
-            if(map.get(el)>n/2)return el;
+        int candidate = 0;
+        int count = 0;
+        for (int num : nums) {
+            if (count == 0) {
+                candidate = num;
+            }
+            if (num == candidate) {
+                count++;
+            } else {
+                count--;
+            }
         }
-        return -1;
+        return candidate;
     }
 }
-//using Hashmap technique whose time complexity is O(log(n))
+//time =O(n) , space= O(1)
+
+
+// class Solution {
+//     public int majorityElement(int[] nums) {
+//         int n=nums.length;
+//         HashMap<Integer,Integer> map =new HashMap<>();
+//         for(int el:nums){
+//             map.put(el,map.getOrDefault(el,0)+1);
+//             if(map.get(el)>n/2)return el;
+//         }
+//         return -1;
+//     }
+// }
+//using Hashmap technique whose time complexity is O(n) but space also O(n) 
 
 // class Solution {
 //     public int majorityElement(int[] nums) {
