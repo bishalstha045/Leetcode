b78943c8875ee4f1088d86d96ed00f326e425402
Bishal Shrestha <bishalstha045@gmail.com>
2026-07-10 15:05:58 +0530

    Time: 2 ms (100%), Space: 46.3 MB (91.55%) - LeetHub

---
+++ 2815-max-pair-sum-in-an-array/2815-max-pair-sum-in-an-array.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int maxSum(int[] nums) {
+        int []freq= new int[10];
+        int ans=-1;
+        for(int num:nums){
+            int getmax=maxDigit(num);
+            if(freq[getmax]!=0){
+                ans=Math.max(ans,freq[getmax]+num);
+            }
+            freq[getmax]=Math.max(freq[getmax],num);
+        }
+        return ans;
+    }
+    public int maxDigit(int n){
+        int max=0;
+        while(n!=0){
+            max=Math.max(max,n%10);
+            n/=10;
+        }
+        return max;
+    }
+}
+// Store the largest number for each maximum digit (0-9) in an array.
+// If another number has the same maximum digit, update the maximum pair sum
+// and keep only the larger number for future comparisons.
