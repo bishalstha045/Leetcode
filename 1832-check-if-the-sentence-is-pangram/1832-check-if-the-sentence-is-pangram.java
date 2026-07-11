f80df7568c456c5b84cc0c3c525007a376966119
Bishal Shrestha <bishalstha045@gmail.com>
2026-07-11 18:17:31 +0530

    Time: 3 ms (35.59%), Space: 43.1 MB (17.48%) - LeetHub

--- 1832-check-if-the-sentence-is-pangram/1832-check-if-the-sentence-is-pangram.java
+++ 1832-check-if-the-sentence-is-pangram/1832-check-if-the-sentence-is-pangram.java
@@ -1,15 +1,35 @@
 class Solution {
     public boolean checkIfPangram(String sentence) {
-        HashSet<Character> alpha= new HashSet<>();
-        for(char el='a' ; el<='z';el++){
-            alpha.add(el);
-        }
+        HashSet<Character>set= new HashSet<>();
         for(char ch:sentence.toCharArray()){
-            alpha.remove(ch);
+            set.add(ch);
         }
-        return alpha.isEmpty()?true:false;
+        return set.size()==26;
     }
 }
+/*
+Approach:
+- Use a HashSet to store all unique characters from the sentence.
+- Since a HashSet does not allow duplicates, each letter is stored only once.
+- If the size of the set becomes 26, then every lowercase alphabet is present,
+  so the sentence is a pangram.
+Time Complexity: O(n)
+Space Complexity: O(1)   // At most 26 letters
+*/
+
+
+// class Solution {
+//     public boolean checkIfPangram(String sentence) {
+//         HashSet<Character> alpha= new HashSet<>();
+//         for(char el='a' ; el<='z';el++){
+//             alpha.add(el);
+//         }
+//         for(char ch:sentence.toCharArray()){
+//             alpha.remove(ch);
+//         }
+//         return alpha.isEmpty()?true:false;
+//     }
+// }
 /*
 Approach:
 - First, add all lowercase letters ('a' to 'z') into a HashSet.
