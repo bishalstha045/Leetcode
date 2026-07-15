6504c6449b405d47064d519c291538c8a1ce8da2
Bishal Shrestha <bishalstha045@gmail.com>
2026-07-15 22:58:08 +0530

    Time: 1 ms (95.68%), Space: 42.6 MB (79.95%) - LeetHub

--- 0392-is-subsequence/0392-is-subsequence.java
+++ 0392-is-subsequence/0392-is-subsequence.java
@@ -1,17 +1,44 @@
 class Solution {
     public boolean isSubsequence(String s, String t) {
-        int i = 0, j = 0;
-        while (i < s.length() && j < t.length()) {
-            if (s.charAt(i) == t.charAt(j)) {
-                i++;
+        int start=0;
+        for(int i=0;i<s.length();i++){
+            int flag=0;
+            for(int j=start;j<t.length();j++){
+                if(t.charAt(j)==s.charAt(i)){
+                    flag=1;
+                    start=j+1;
+                    break;
+                }
             }
-            j++;
+            if(flag==0)return false;
         }
-        return i == s.length();
+        return true;
     }
 }
+/*
+Approach:
+For each character in s, search for its first matching character in t starting from the last matched position.
+If a match is found, update the starting index for the next search.
+If any character cannot be matched, return false.
+If all characters are matched in order, return true.
+*/
+//time(O(m*n) space=O(1))
+
+// class Solution {
+//     public boolean isSubsequence(String s, String t) {
+//         int i = 0, j = 0;
+//         while (i < s.length() && j < t.length()) {
+//             if (s.charAt(i) == t.charAt(j)) {
+//                 i++;
+//             }
+//             j++;
+//         }
+//         return i == s.length();
+//     }
+// }
 /*
 Use two pointers to traverse both strings.
 If characters match, move both pointers; otherwise, move only the pointer of t.
 If all characters of s are matched in order, return true; otherwise, return false.
 */
+//time(O(n) space=O(1))
