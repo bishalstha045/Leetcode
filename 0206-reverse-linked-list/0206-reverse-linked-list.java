cc10049652a3a1ea84e601be43641fec70f66da6
Bishal Shrestha <bishalstha045@gmail.com>
2026-07-17 18:30:29 +0530

    Time: 0 ms (100%), Space: 44.2 MB (50.48%) - LeetHub

--- 0206-reverse-linked-list/0206-reverse-linked-list.java
+++ 0206-reverse-linked-list/0206-reverse-linked-list.java
@@ -10,23 +10,49 @@
  */
 class Solution {
     public ListNode reverseList(ListNode head) {
-        Stack<Integer> s= new Stack<>();
-        ListNode temp=head;
-        //firstly store data in stacks
-        while(temp!=null){
-            s.push(temp.val);
-            temp=temp.next;
-        }
-        //now put values back in reverse order
-        temp=head;
-        while(temp!=null){
-            temp.val=s.peek();
-            s.pop();
-            temp=temp.next;
-        }
-        return head;
+      ListNode temp=head;
+      ListNode prev=null;
+      while(temp!=null){
+        ListNode next=temp.next; // Save next node
+        temp.next=prev;// Reverse link
+        prev=temp;// Move prev forward
+        temp=next;// Move curr forward
+      }
+      return prev;
     }
 }
+/*
+Approach:
+- Use three pointers: prev, curr, and next. (curr means temp)
+- Traverse the list while storing the next node before changing any links.
+- Reverse the current node's next pointer to point to the previous node.
+- Move all pointers one step forward and continue until the end of the list.
+- At the end, prev points to the new head of the reversed linked list.
+Time Complexity: O(n)
+Space Complexity: O(1)
+*/
+
+
+
+// class Solution {
+//     public ListNode reverseList(ListNode head) {
+//         Stack<Integer> s= new Stack<>();
+//         ListNode temp=head;
+//         //firstly store data in stacks
+//         while(temp!=null){
+//             s.push(temp.val);
+//             temp=temp.next;
+//         }
+//         //now put values back in reverse order
+//         temp=head;
+//         while(temp!=null){
+//             temp.val=s.peek();
+//             s.pop();
+//             temp=temp.next;
+//         }
+//         return head;
+//     }
+// }
 /*
 Approach:
 - Traverse the linked list and push each node's value into a stack.
