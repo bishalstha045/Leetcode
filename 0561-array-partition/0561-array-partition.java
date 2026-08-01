856a427c06bcd83ea051f92307a314308250dc8d
Bishal Shrestha <bishalstha045@gmail.com>
2026-08-02 00:00:02 +0530

    Time: 17 ms (81.32%), Space: 49.9 MB (17.95%) - LeetHub

--- 0561-array-partition/0561-array-partition.java
+++ 0561-array-partition/0561-array-partition.java
@@ -1,13 +1,13 @@
-
 class Solution {
     public int arrayPairSum(int[] nums) {
         Arrays.sort(nums);
-
         int sum = 0;
         for (int i = 0; i < nums.length; i += 2) {
             sum += nums[i];
         }
-
         return sum;
     }
 }
+// First sort the array so that adjacent numbers form the best possible pairs.
+// In every pair, the first element is the smaller one.
+// Adding all the smaller elements gives the maximum possible answer.
