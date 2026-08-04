629fffcee67fba82091f816dadce2a8a801148e1
Bishal Shrestha <bishalstha045@gmail.com>
2026-08-04 22:35:38 +0530

    Time: 0 ms (100%), Space: 44.6 MB (40.4%) - LeetHub

--- 1539-kth-missing-positive-number/1539-kth-missing-positive-number.java
+++ 1539-kth-missing-positive-number/1539-kth-missing-positive-number.java
@@ -3,11 +3,11 @@ class Solution {
         int low=0;
         int high=arr.length-1;
         while(low<=high){
-            int mid = low + (high- low)/2;
-            int actual_no=mid+1;
-            int missing_no=arr[mid]-actual_no;
-            if(missing_no >= k) high=mid-1;
-            else low=mid+1;
+            int mid=low+(high-low)/2;
+            int actual=mid+1;
+            int missing=arr[mid]-actual;
+            if(missing<k)low=mid+1;
+            else high=mid-1;
         }
         return low+k;
     }
@@ -17,15 +17,11 @@ class Solution {
 // Intuition:
 // At index i, expected number = i + 1
 // Missing numbers till index i = arr[i] - (i + 1)
-//
 // We use binary search to find the smallest index where
 // missing numbers >= k.
-//
 // If missing >= k → move left
 // Else → move right
-//
 // Finally, answer = k + low
 // (low represents how many elements are before kth missing)
-//
 // Time Complexity: O(log n)
 // Space Complexity: O(1)
