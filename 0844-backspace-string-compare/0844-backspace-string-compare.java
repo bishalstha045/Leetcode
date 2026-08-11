7d832d8df158c07851839c93b1175f6a7a2bb3d7
Bishal Shrestha <bishalstha045@gmail.com>
2026-08-11 18:28:56 +0530

    Time: 2 ms (59.75%), Space: 43.2 MB (39.61%) - LeetHub

--- 0844-backspace-string-compare/0844-backspace-string-compare.java
+++ 0844-backspace-string-compare/0844-backspace-string-compare.java
@@ -1,17 +1,24 @@
 class Solution {
     public boolean backspaceCompare(String s, String t) {
-        return build(s).equals(build(t));
+        return method(s).equals(method(t));
     }
-
-    private String build(String s) {
-        Stack<Character> st = new Stack<>();
-        for (char c : s.toCharArray()) {
-            if (c == '#') {
-                if (!st.isEmpty()) st.pop();
-            } else st.push(c);
+    public String method(String s){
+        Stack<Character> st=new Stack<>();
+        for(char c:s.toCharArray()){
+            if(c=='#'){
+               if(!st.isEmpty()) st.pop();
+            }
+            else st.push(c);
         }
-        StringBuilder sb = new StringBuilder();
-        for (char c : st) sb.append(c);
+        StringBuilder sb=new StringBuilder();
+        for(char c:st){
+            sb.append(c);
+        }
         return sb.toString();
     }
 }
+// First I build the final string after applying all the backspaces.
+// I use a stack where normal characters are pushed and '#' removes
+// the last character if the stack is not empty.
+// Then I convert the stack into a string and compare the final
+// strings of s and t. If both are same, return true.
