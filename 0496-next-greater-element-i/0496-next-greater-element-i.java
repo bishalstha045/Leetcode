1d5120d626c1f322df8f11a8d8d8297162b6d533
Bishal Shrestha <bishalstha045@gmail.com>
2026-08-15 11:48:06 +0530

    Time: 3 ms (92.67%), Space: 45.6 MB (11.03%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.java
+++ 0496-next-greater-element-i/0496-next-greater-element-i.java
@@ -1,24 +1,22 @@
 class Solution {
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
-        Stack<Integer> stack = new Stack<>();
-        HashMap<Integer, Integer> map = new HashMap<>();
-        // Find next greater element for nums2
-        for (int num : nums2) {
-            while (!stack.isEmpty() && num > stack.peek()) {
-                map.put(stack.pop(), num);
-            }
-            stack.push(num);
+       int[]ans=new int[nums1.length];
+       Stack<Integer>st=new Stack<>();
+       HashMap<Integer,Integer> map=new HashMap<>();
+       for(int i=nums2.length-1;i>=0;i--){
+        while(!st.isEmpty()&& nums2[i]>=st.peek()){
+            st.pop();
         }
-        // Remaining elements have no greater element
-        while (!stack.isEmpty()) {
-            map.put(stack.pop(), -1);
-        }
-        // Build answer for nums1
-        int[] ans = new int[nums1.length];
-
-        for (int i = 0; i < nums1.length; i++) {
-            ans[i] = map.get(nums1[i]);
-        }
-        return ans;
+        if(st.isEmpty())map.put(nums2[i],-1);
+        else map.put(nums2[i],st.peek());
+        st.push(nums2[i]);
+       }
+       for(int i=0;i<nums1.length;i++){
+        ans[i]=map.get(nums1[i]);
+       }
+       return ans;
     }
 }
+/*
+I used a monotonic decreasing stack to find the next greater element for each element in nums2. I traverse nums2 from right to left and remove all smaller or equal elements from the stack because they cannot be the next greater element. If the stack is empty, I store -1 in the HashMap, otherwise I store the top of the stack as the next greater element. The HashMap stores each element of nums2 along with its next greater element. Finally, I traverse nums1 and use map.get() to get the answer for each element. This solution takes O(n + m) time and O(n) extra space.
+ */
