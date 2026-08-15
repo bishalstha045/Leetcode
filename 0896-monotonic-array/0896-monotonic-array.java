3e417b6c1e7b060b1151d5e2a0f7a2ccaae765e8
Bishal Shrestha <bishalstha045@gmail.com>
2026-08-15 22:12:15 +0530

    Time: 4 ms (3.74%), Space: 85.2 MB (60.34%) - LeetHub

--- 0896-monotonic-array/0896-monotonic-array.java
+++ 0896-monotonic-array/0896-monotonic-array.java
@@ -12,25 +12,3 @@ class Solution {
         return false;
     }
 }
-
-
-// class Solution {
-//     public boolean isMonotonic(int[] nums) {
-//         for(int i=0;i<nums.length;i++){
-//            if(isDecreasing(nums) ||isIncreasing(nums))return true;
-//         }
-//         return false;
-//     }
-//     boolean isDecreasing(int []nums){
-//         for(int i=1;i<nums.length;i++){
-//             if(nums[i]>nums[i-1]) return false;
-//         }
-//      return true;
-//     }
-//      boolean isIncreasing(int []nums){
-//         for(int i=1;i<nums.length;i++){
-//             if(nums[i]<nums[i-1]) return false;
-//         }
-//      return true;
-//     }
-// }
