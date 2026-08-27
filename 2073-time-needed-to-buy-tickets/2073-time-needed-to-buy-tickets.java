a1e8812df86bef580a6c495a61091235aae5c023
Bishal Shrestha <bishalstha045@gmail.com>
2026-08-27 13:02:19 +0530

    Time: 9 ms (42.15%), Space: 43 MB (75.75%) - LeetHub

--- 2073-time-needed-to-buy-tickets/2073-time-needed-to-buy-tickets.java
+++ 2073-time-needed-to-buy-tickets/2073-time-needed-to-buy-tickets.java
@@ -1,16 +1,44 @@
 class Solution {
     public int timeRequiredToBuy(int[] tickets, int k) {
-        int time = 0;
-        for (int i = 0; i < tickets.length; i++) {
-            if (i <= k) {
-                time += Math.min(tickets[i], tickets[k]);
-            } else {
-                time += Math.min(tickets[i], tickets[k] - 1);
+        Queue<Integer> q=new ArrayDeque<>();
+        for(int i=0;i<tickets.length;i++){
+            q.offer(i);
+        }
+        int time=0;
+        while(!q.isEmpty()){
+            int i=q.poll();
+            tickets[i]--;
+            time++;
+            if(tickets[i]>0){
+                q.offer(i);
+            }
+            else if(i==k && tickets[i]==0){
+                return time;
             }
         }
-        return time;
+        return -1; 
     }
 }
+/*
+
+I use a queue to store each person's index and simulate buying tickets one by one. Each person buys one ticket and goes back to the queue if they still have tickets left. When person k buys their last ticket, we return the total time taken.
+ */
+
+
+
+// class Solution {
+//     public int timeRequiredToBuy(int[] tickets, int k) {
+//         int time = 0;
+//         for (int i = 0; i < tickets.length; i++) {
+//             if (i <= k) {
+//                 time += Math.min(tickets[i], tickets[k]);
+//             } else {
+//                 time += Math.min(tickets[i], tickets[k] - 1);
+//             }
+//         }
+//         return time;
+//     }
+// }
 
 /*
 Approach:
